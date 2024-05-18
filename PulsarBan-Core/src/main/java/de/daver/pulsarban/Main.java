dc644d03027962c7a81b594e375204726f09ece7
DaverMc <pommesesser61@gmail.com>
2024-05-18 23:56:56 +0200

    Initial Commit for Modules

--- PulsarBan-Core/src/main/java/de/daver/pulsarban/Main.java
+++ PulsarBan-Core/src/main/java/de/daver/pulsarban/Main.java
@@ -3,7 +3,7 @@ package de.daver.pulsarban;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Starting Ban system!");
+        System.out.println("Starting PulsarBan-Core...");
     }
 
 }
